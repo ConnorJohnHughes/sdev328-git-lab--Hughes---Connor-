97fec02b41362eb03e0f2675b488749c2c5fc891
Connor <connorjohnhughes@gmail.com>
2026-01-06 11:12:38 -0800

    Added line to end of the poem

--- poem.js
+++ poem.js
@@ -43,6 +43,8 @@ const poem = `
     Who brave the night and make it work.
     We'll meet again at dawn's first ray,
     With coffee brewed and bugs to slay.
+
+    All of that to make "Hello World" work.
 `;
 
 console.log(poem);
